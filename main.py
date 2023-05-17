bfe52ca99f52f1421d86b776961135c4a0efd7c9
grczk <130426781+grczk@users.noreply.github.com>
2023-05-17 08:58:53 +0200

    Added loading data for the other 2 data sets
    yeast & ecoli

--- main.py
+++ main.py
@@ -3,11 +3,25 @@ import pandas as pd
 import numpy as np
 
 def load_wine():
-    data = pd.read_csv("data/wine.data", names=["class","alcohol", "malic_acid", "ash", "alcalnity", "magnesium", "total_phenols", "flavanoids", "nonflavanoids_phenols", "proanthocyanins", "color_intensity", "hue", "od_of_diluted_wines", "proline"])
+    data = pd.read_csv("data/wine.data", names=["class", "alcohol", "malic_acid", "ash", "alcalnity", "magnesium", "total_phenols", "flavanoids", "nonflavanoids_phenols", "proanthocyanins", "color_intensity", "hue", "od_of_diluted_wines", "proline"])
     #print(data)
     classes = data["class"].to_numpy()
-    features = data.drop("class",axis=1).to_numpy()
-    return features,classes
+    features = data.drop("class", axis=1).to_numpy()
+    return features, classes
+
+def load_yeast():
+    data = pd.read_csv("data/yeast.data", delim_whitespace=True, names=["name", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc", "class"])
+    #print(data)
+    classes = data["class"].to_numpy()
+    features = data.drop(["class", "name"], axis=1).to_numpy()
+    return features, classes
+
+def load_ecoli():
+    data = pd.read_csv("data/ecoli.data", delim_whitespace=True, names=["name", "mcg", "gvh", "lip", "chg", "aac", "alm1", "alm2", "class"])
+    #print(data)
+    classes = data["class"].to_numpy()
+    features = data.drop(["class", "name"], axis=1).to_numpy()
+    return features, classes
 
 def load_iris():
     data = pd.read_csv("data/iris.data", names=["sepal_length", "sepal_width", "petal_length", "petal_width", "class"])
@@ -25,19 +39,19 @@ def evaluate(clusters, labels):
         print("\n")
     
 
-def clustering(kmeans_pp, data):
+def clustering(kmeans_pp, data, num_clusters):
     features, classes = data
     intra_class_variance = []
     for i in range(250):
-        assignments, centroids, error = k_means(features, 3, kmeans_pp)
+        assignments, centroids, error = k_means(features, num_clusters, kmeans_pp)
         evaluate(assignments, classes)
         intra_class_variance.append(error)
-    print(f"Mean intra-class variance: {np.mean(intra_class_var iance)}")
+    print(f"Mean intra-class variance: {np.mean(intra_class_variance)}")
 
 if __name__=="__main__":
     print("K-means++ Wine data set")
-    clustering(kmeans_pp=True, data=load_wine())
+    clustering(kmeans_pp=True, data=load_wine(), num_clusters=3)
     print("K-means++ Tools data set")
-    clustering(kmeans_pp=True, data=load_tools())
+    clustering(kmeans_pp=True, data=load_yeast(), num_clusters=10)
     print("K-means++ 3rd data set")
-    clustering(kmeans_pp=True, data=load_3rddataset())
+    clustering(kmeans_pp=True, data=load_ecoli(), num_clusters=8)
