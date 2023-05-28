25da935946d593366de114bf833febc45bac0a7c
grczk <130426781+grczk@users.noreply.github.com>
2023-05-28 08:50:25 +0200

    Added graphical representation of clustering after dimensionality reduction

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from matplotlib import pyplot as plt
-
 from k_means import k_means
-from dbscan import dbscan, calculate_epsilon
-from hierarchical import  hierarchical
+from dbscan import dbscan
+from hierarchical import hierarchical
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import numpy as np
+
 
 def load_wine():
     data = pd.read_csv("data/wine.data", names=["class", "alcohol", "malic_acid", "ash", "alcalnity", "magnesium", "total_phenols", "flavanoids", "nonflavanoids_phenols", "proanthocyanins", "color_intensity", "hue", "od_of_diluted_wines", "proline"])
@@ -13,12 +16,14 @@ def load_wine():
     features = data.drop("class", axis=1).to_numpy()
     return features, classes
 
+
 def load_yeast():
     data = pd.read_csv("data/yeast.data", delim_whitespace=True, names=["name", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc", "class"])
     #print(data)
     classes = data["class"].to_numpy()
     features = data.drop(["class", "name"], axis=1).to_numpy()
     return features, classes
+
 
 def load_ecoli():
     data = pd.read_csv("data/ecoli.data", delim_whitespace=True, names=["name", "mcg", "gvh", "lip", "chg", "aac", "alm1", "alm2", "class"])
@@ -27,6 +32,7 @@ def load_ecoli():
     features = data.drop(["class", "name"], axis=1).to_numpy()
     return features, classes
 
+
 def evaluate(clusters, labels):
     for cluster in np.unique(clusters):
         labels_in_cluster = labels[clusters==cluster]
@@ -34,7 +40,28 @@ def evaluate(clusters, labels):
         for label_type in np.unique(labels):
             print(f"Num of {label_type}: {np.sum(labels_in_cluster==label_type)}")
         print("\n")
-    
+
+
+def plot_clusters(data, models, results):
+    pca = PCA(n_components=2)  # dimensionality reduction
+    reduced_features = pca.fit_transform(data)
+
+    le = LabelEncoder()  # converting string class names to numbers
+    models_encoded = le.fit_transform(models)
+
+    plt.subplot(121)
+    num_clusters = len(np.unique(models_encoded))
+    cmap_colors = colors.ListedColormap(['C{}'.format(i) for i in range(num_clusters)])  # more distinctive colours
+    plt.scatter(reduced_features[:, 0], reduced_features[:, 1], c=models_encoded, cmap=cmap_colors)
+    plt.title('Model')
+
+    plt.subplot(122)
+    plt.scatter(reduced_features[:, 0], reduced_features[:, 1], c=results, cmap=cmap_colors)
+    plt.title('Result')
+    plt.suptitle("Comparison", fontsize=16)
+    plt.tight_layout()
+    plt.show()
+
 
 def kmeans_clustering(data, num_clusters):
     features, classes = data
@@ -43,26 +70,24 @@ def kmeans_clustering(data, num_clusters):
         assignments, centroids, error = k_means(features, num_clusters)
         intra_class_variance.append(error)
     evaluate(assignments, classes)
+    plot_clusters(features, classes, assignments)
     print(f"Mean intra-class variance: {np.mean(intra_class_variance)}")
+
 
 def dbscan_clustering(data, eps):
     features, classes = data
     num_of_points = features.shape[1] # number of features
     assignments = dbscan(features, eps, num_of_points)
-    evaluate_dbscane(assignments, classes)
+    evaluate(assignments, classes)
+    plot_clusters(features, classes, assignments)
 
-def evaluate_dbscane(assignments, classes):
-    for cluster in np.unique(assignments):
-        labels_in_cluster = classes[assignments==cluster]
-        print(f"Cluster: {cluster}")
-        for label_type in np.unique(classes):
-            print(f"Num of {label_type}: {np.sum(labels_in_cluster==label_type)}")
-        print("\n")
 
 def hierarchical_clustering(data, clusters):
     features, classes = data
     assignments = hierarchical(features, clusters)
     evaluate(assignments, classes)
+    plot_clusters(features, classes, assignments)
+
 
 if __name__=="__main__":
     print("Clustering Algorithms for 3 Datasets:\n")
